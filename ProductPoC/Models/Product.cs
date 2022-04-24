101a6579f9c6ffd96f9e9dc34afc75775bab9c57
Elango <srielango@yahoo.com>
2022-04-24 07:49:42 +0530

    Add project files.

---
+++ ProductPoC/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductPoC.Models
+{
+    public class Product
+    {
+        public string? Id { get; set; }
+        public string? ProductName { get; set; }
+        public string? Category { get; set; }
+        public string? Price { get; set; }
+        public string? Avatar { get; set; }
+        public string? CreatedAt { get; set; }
+
+    }
+}
---
+++ ProductPoC/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProductPoC.Models;
+using Newtonsoft.Json;
+
+namespace ProductPoC.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public List<Product>? Products { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGetAsync()
+        {
+            await LoadProductsAsync();
+        }
+
+        private async Task LoadProductsAsync()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://6264155ba6adc673188d5a95.mockapi.io/Products"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    //Products = JsonSerializer.Deserialize<List<Product>>(apiResponse); 
+                    Products = JsonConvert.DeserializeObject<List<Product>>(apiResponse);
+                }
+            }
+        }
+    }
+}
